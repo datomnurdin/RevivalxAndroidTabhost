274dbd452c2fc33c1087563e8d450a856039aab3
Mohammad Nurdin <mohammadnrdn@gmail.com>
2015-05-09 17:11:08 +0800

    Add fragments and activity.

---
+++ app/src/main/java/com/revivalx/revivalxandroidtabhost/FirstFragment.java
@@ -0,0 +1,39 @@
+package com.revivalx.revivalxandroidtabhost;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.first_layout,
+                null);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+    }
+    //
+
+}
---
+++ app/src/main/java/com/revivalx/revivalxandroidtabhost/FourthFragment.java
@@ -0,0 +1,42 @@
+package com.revivalx.revivalxandroidtabhost;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by MNurdin on 5/8/15.
+ */
+public class FourthFragment extends Fragment {
+
+    public FourthFragment() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.fourth_layout,
+                null);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+    }
+    //
+
+}
